e561a98d6579e8ef47b83d26f5c16d6042670b24
dev9fe8891 <dev9fe8891@gmail.com>
2026-07-13 10:54:02 +0330

    feat: enhance HomePage with search functionality and loading state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,33 +1,50 @@
 import axios from "axios";
-import { Link, useLoaderData } from "react-router-dom";
+import { Form, Link, useLoaderData, useNavigation } from "react-router-dom";
 
 const searchMealByNameUrl =
   "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-export const loader = async () => {
-  const { data } = await axios.get(searchMealByNameUrl);
-  return { meals: data.meals || [] };
+export const loader = async ({ request }) => {
+  const searchTerm = new URL(request.url).searchParams.get("q") ?? "";
+  const { data } = await axios.get(`${searchMealByNameUrl}${searchTerm}`);
+  return { meals: data.meals || [], searchTerm };
 };
 
 function HomePage() {
-  const { meals } = useLoaderData();
+  const { meals, searchTerm } = useLoaderData();
 
-  return meals.map((meal) => {
-    const {
-      idMeal: id,
-      strMeal: name,
-      strMealThumb: image,
-      strCountry: country,
-    } = meal;
-    return (
-      <div key={id}>
-        <div>{name}</div>
-        <img src={image} alt={name} />
-        <div>{country}</div>
-        <Link to={`/meals/${id}`}>details</Link>
-      </div>
-    );
-  });
+  const navigation = useNavigation();
+
+  const isSearching =
+    navigation.state === "loading" &&
+    navigation.location?.search.includes("q=");
+
+  return (
+    <div>
+      <Form key={searchTerm}>
+        <input type="search" name="q" defaultValue={searchTerm} />
+        <button type="submit" disabled={isSearching}>
+          {isSearching ? "Searching..." : "Search"}
+        </button>
+      </Form>
+      {meals.map((meal) => {
+        const {
+          idMeal: id,
+          strMeal: name,
+          strMealThumb: image,
+          strCountry: country,
+        } = meal;
+        return (
+          <div key={id}>
+            <div>{name}</div>
+            <img src={image} alt={name} />
+            <div>{country}</div>
+            <Link to={`/meals/${id}`}>details</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default HomePage;
