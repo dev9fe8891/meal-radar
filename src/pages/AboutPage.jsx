99577a1c954e945164d44b51b694a577be231166
dev9fe8891 <dev9fe8891@gmail.com>
2026-07-14 11:46:09 +0330

    feat: update AboutPage with detailed description

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,16 @@
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div>About Us</div>
+      <div>
+        Find inspiration for your next meal with Meal Radar. Our platform makes
+        it easy to explore thousands of recipes, view clear ingredients, and
+        follow step-by-step instructions. Built for performance, Meal Radar
+        transforms recipe searching into a simple, delightful experience for
+        every home cook.
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
