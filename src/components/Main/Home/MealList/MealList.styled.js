05bcc7e9a0ed0c90e9fd8745f3b1dea018ae2250
dev9fe8891 <dev9fe8891@gmail.com>
2026-07-20 11:50:24 +0330

    feat: update MealList styling for responsiveness

--- src/components/Main/Home/MealList/MealList.styled.js
+++ src/components/Main/Home/MealList/MealList.styled.js
@@ -3,9 +3,15 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   &.home__meals {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(260px, 320px));
+    grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
     gap: var(--space-6);
     justify-content: center;
+  }
+
+  @media (min-width: 768px) {
+    &.home__meals {
+      grid-template-columns: repeat(auto-fit, minmax(260px, 320px));
+    }
   }
 `;
 
